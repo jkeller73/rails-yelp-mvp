318bf7e3d0176a1b54da5aa3fb549af5e0150d6b
Juliette Keller <kellerjuliette@hotmail.com>
2019-11-13 15:41:30 +0000

    completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  # def show
+  #   @review = Review.find(params[:id])
+  # end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:content, :rating)
+end
